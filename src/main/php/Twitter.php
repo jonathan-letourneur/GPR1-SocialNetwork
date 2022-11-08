df9763d792c080f6a5b78cec7d7fe6f1cf3acb68
habbst1 <habbst1@gmail.com>
2022-11-08 15:34:47 +0100

    corriged file for the test

--- src/main/php/Twitter.php
+++ src/main/php/Twitter.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace SocialNetwork;
 
@@ -8,42 +8,67 @@ require 'IObservable.php';
 
 class Twitter implements IObservable
 {
-    //region private attributes
-    private array $observers = array();
-    //endregion private attributes
+    private array $observers = [];
+    private array $twits = [];
 
-    public function __construct(array $observers = array())
+    public function __construct(array $observers = [])
     {
-        throw new RuntimeException();
+        //TODO Review - Single Responsibility Principle
+        $this->observers = $observers;
     }
 
-    public function subscribe(array $observers):void
-    {
-        throw new RuntimeException();
-    }
-
-    public function unsubscribe(IObserver $observer):void
-    {
-        throw new RuntimeException();
-    }
-
-    public function notifyObservers():void
-    {
-        throw new RuntimeException();
-    }
-
-    public function getObservers():array
+    public function getObservers(): array
     {
         return $this->observers;
     }
 
-    public function getTwits():array
+    public function getTwits(): array
     {
-        throw new RuntimeException();
+        return $this->twits;
+    }
+
+    public function subscribe(array $observers): void
+    {
+        //TODO Review be consistent with indentation style ("{")
+        foreach ($observers as $observer) {
+            if (in_array($observer, $this->observers, true)) {
+                throw new SubscriberAlreadyExistsException();
+            }
+            $this->observers[] = $observer;
+        }
+    }
+
+    public function unsubscribe(IObserver $observer): void
+    {
+        if (count($this->observers) === 0) {
+            throw new EmptyListOfSubscribersException();
+        }
+        if (!in_array($observer, $this->observers, true)) {
+            throw new SubscriberNotFoundException();
+        }
+        unset($this->observers[array_search($observer, $this->observers)]);
+    }
+
+    public function notifyObservers(): void
+    {
+        if (count($this->observers) === 0) {
+            throw new EmptyListOfSubscribersException();
+        }
     }
 }
 
-class TwitterException extends RuntimeException { }
-class EmptyListOfSubscribersException extends TwitterException { }
-class SubscriberAlreadyExistsException extends TwitterException { }
-class SubscriberNotFoundException extends TwitterException { }
+class TwitterException extends RuntimeException
+{
+}
+
+class EmptyListOfSubscribersException extends TwitterException
+{
+}
+
+class SubscriberAlreadyExistsException extends TwitterException
+{
+}
+
+class SubscriberNotFoundException extends TwitterException
+{
+}
